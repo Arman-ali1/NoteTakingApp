dff050e192c6eaace4e00ab60cb5775517500681
Arman Ali <armnsrn173200@gmail.com>
2024-01-10 03:34:19 +0530

    Update API proxy URL in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://notetakingapp-backend.onrender.com",
+      "/api": "https://notetakingapp-backend.onrender.com/",
     },
   },
   plugins: [react()],
